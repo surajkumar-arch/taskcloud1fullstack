66d6fb5a9c44e956f9b7c970f16b160248a341ee
Suraj Kumar Jaiswal <sj029033@gmail.com>
2025-09-10 00:34:53 +0530

    Update index.js

--- server/utils/index.js
+++ server/utils/index.js
@@ -8,8 +8,8 @@ expiresIn: "1d",
 // 🛠 Cookie setup (works on localhost too)
 res.cookie("token", token, {
 httpOnly: true,
-secure: false,     // force false in dev, change to true in prod (with HTTPS)
-sameSite: "lax",   // use "none" only if frontend is on different domain
+secure: process.env.NODE_ENV === "production",     // force false in dev, change to true in prod (with HTTPS)
+sameSite: "none",   // use "none" only if frontend is on different domain
 maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
 });
 
