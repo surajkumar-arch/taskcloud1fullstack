3c2684e711b970ccf5a0179a70b889fb56955174
Suraj Kumar Jaiswal <sj029033@gmail.com>
2025-09-09 11:14:28 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,35 +5,43 @@ import createJWT from "../utils/index.js";
 
 // POST request - login user
 const loginUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-  if (!user) {
-    return res
-      .status(401)
-      .json({ status: false, message: "Invalid email or password." });
-  }
+    if (!user) {
+      return res
+        .status(401)
+        .json({ status: false, message: "Invalid email or password." });
+    }
 
-  if (!user?.isActive) {
-    return res.status(401).json({
-      status: false,
-      message: "User account has been deactivated, contact the administrator",
-    });
-  }
+    if (!user?.isActive) {
+      return res.status(401).json({
+        status: false,
+        message:
+          "User account has been deactivated, contact the administrator",
+      });
+    }
 
-  const isMatch = await user.matchPassword(password);
+    const isMatch = await user.matchPassword(password);
 
-  if (user && isMatch) {
-    const token = createJWT(res, user._id);
+    if (user && isMatch) {
+      const token = createJWT(res, user._id);
 
-    user.password = undefined;
+      user.password = undefined;
 
-    res.status(200).json(user,token);
-  } else {
-    return res
-      .status(401)
-      .json({ status: false, message: "Invalid email or password" });
+      res.status(200).json({ user, token });  // ✅ Proper format
+    } else {
+      return res
+        .status(401)
+        .json({ status: false, message: "Invalid email or password" });
+    }
+  } catch (error) {
+    console.error("Login Error:", error);
+    res
+      .status(500)
+      .json({ status: false, message: "Internal server error", error: error.message });
   }
 });
 
@@ -59,11 +67,11 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
-      createJWT(res, user._id);
+    createJWT(res, user._id);
 
     user.password = undefined;
 
-    res.status(201).json(user);
+    res.status(201).json({ user });
   } else {
     return res
       .status(400)
@@ -71,7 +79,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-// POST -  Logout user / clear cookie
+// POST - Logout user / clear cookie
 const logoutUser = (req, res) => {
   res.cookie("token", "", {
     httpOnly: true,
@@ -79,22 +87,6 @@ const logoutUser = (req, res) => {
   });
   res.status(200).json({ message: "Logged out successfully" });
 };
-
-// @GET -   Get user profile
-// const getUserProfile = asyncHandler(async (req, res) => {
-//   const { userId } = req.user;
-
-//   const user = await User.findById(userId);
-
-//   user.password = undefined;
-
-//   if (user) {
-//     res.json({ ...user });
-//   } else {
-//     res.status(404);
-//     throw new Error("User not found");
-//   }
-// });
 
 const getTeamList = asyncHandler(async (req, res) => {
   const { search } = req.query;
@@ -117,7 +109,6 @@ const getTeamList = asyncHandler(async (req, res) => {
   res.status(201).json(user);
 });
 
-// @GET  - get user notifications
 const getNotificationsList = asyncHandler(async (req, res) => {
   const { userId } = req.user;
 
@@ -131,7 +122,6 @@ const getNotificationsList = asyncHandler(async (req, res) => {
   res.status(200).json(notice);
 });
 
-// @GET  - get user task status
 const getUserTaskStatus = asyncHandler(async (req, res) => {
   const tasks = await User.find()
     .populate("tasks", "title stage")
@@ -140,7 +130,6 @@ const getUserTaskStatus = asyncHandler(async (req, res) => {
   res.status(200).json(tasks);
 });
 
-// @GET  - get user notifications
 const markNotificationRead = asyncHandler(async (req, res) => {
   try {
     const { userId } = req.user;
@@ -161,11 +150,11 @@ const markNotificationRead = asyncHandler(async (req, res) => {
     }
     res.status(201).json({ status: true, message: "Done" });
   } catch (error) {
-    console.log(error);
+    console.error("Mark Notification Error:", error);
+    res.status(500).json({ status: false, message: "Server error" });
   }
 });
 
-// PUT - Update user profile
 const updateUserProfile = asyncHandler(async (req, res) => {
   const { userId, isAdmin } = req.user;
   const { _id } = req.body;
@@ -181,7 +170,6 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 
   if (user) {
     user.name = req.body.name || user.name;
-    // user.email = req.body.email || user.email;
     user.title = req.body.title || user.title;
     user.role = req.body.role || user.role;
 
@@ -199,7 +187,6 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-// PUT - active/disactivate user profile
 const activateUserProfile = asyncHandler(async (req, res) => {
   const { id } = req.params;
 
@@ -226,11 +213,10 @@ const activateUserProfile = asyncHandler(async (req, res) => {
 const changeUserPassword = asyncHandler(async (req, res) => {
   const { userId } = req.user;
 
-  // Remove this condition
   if (userId === "65ff94c7bb2de638d0c73f63") {
     return res.status(404).json({
       status: false,
-      message: "This is a test user. You can not chnage password. Thank you!!!",
+      message: "This is a test user. You can not change password. Thank you!!!",
     });
   }
 
@@ -245,14 +231,13 @@ const changeUserPassword = asyncHandler(async (req, res) => {
 
     res.status(201).json({
       status: true,
-      message: `Password chnaged successfully.`,
+      message: Password changed successfully.,
     });
   } else {
     res.status(404).json({ status: false, message: "User not found" });
   }
 });
 
-// DELETE - delete user account
 const deleteUserProfile = asyncHandler(async (req, res) => {
   const { id } = req.params;
 
