d69d32cf9db2ae0d26855d63b856de566f2bf1df
Suraj Kumar Jaiswal <sj029033@gmail.com>
2025-09-10 13:14:51 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://taskram.netlify.app","http://localhost:3000","http://localhost:3001"],
+    origin: ["https://task-mangerjw.netlify.app","http://localhost:3000","http://localhost:3001"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
